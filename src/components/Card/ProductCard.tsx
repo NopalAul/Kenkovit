92260e8df41ea351c6d1c0d2eb60038cc67649fb
NopalAul <86586142+NopalAul@users.noreply.github.com>
2025-01-29 21:47:58 +0700

    feat: bottom text for product card

--- src/components/Card/ProductCard.tsx
+++ src/components/Card/ProductCard.tsx
@@ -11,11 +11,15 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ id, name, image, bgColor }) => {
   return (
     <Link href={`/product/${id}`} passHref>
-      <div
-        className={`cursor-pointer w-[290px] h-[350px] p-6 rounded-2xl shadow-cardCombined ${bgColor} flex flex-col items-center justify-center transition-transform hover:scale-105`}
-      >
-        <Image src={image} alt={name} height={300} width={300} className="h-60 w-auto" />
-        <h3 className="mt-4 text-center font-semibold">{name}</h3>
+      <div className={`cursor-pointer w-[290px] h-[350px] rounded-2xl shadow-cardCombined ${bgColor} flex flex-col items-center justify-between transition-transform hover:scale-105`}>
+        <div className="px-6 pt-12">
+          <Image src={image} alt={name} height={300} width={300} className="h-60 w-auto" />
+        </div>
+        {/* <h3 className="mt-4 text-center font-semibold">{name}</h3> */}
+        <div className={`flex h-[40px] bg-purple1 w-full flex-col items-center justify-center px-4 rounded-b-2xl shadow-card`}>
+          <h3 className="text-center font-medium text-white">{name}</h3>
+          {/* <h4 className="text-white">{desc}</h4> */}
+        </div>
       </div>
     </Link>
   );
