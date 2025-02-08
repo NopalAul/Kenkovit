36eaf2aadb9de4eb752ed94f90829f2dfd067de1
NopalAul <86586142+NopalAul@users.noreply.github.com>
2025-02-08 11:59:15 +0700

    feat: product carousel mobile size

--- src/components/Card/ProductCard.tsx
+++ src/components/Card/ProductCard.tsx
@@ -15,9 +15,9 @@ const ProductCard: React.FC<ProductCardProps> = ({ id }) => {
 
   return (
     <Link href={`/product/${id}`} passHref>
-      <div className={`cursor-pointer md:w-[290px] md:h-[350px] rounded-2xl shadow-card md:shadow-cardCombined ${product.bgColor} flex flex-col items-center justify-between transition-transform hover:scale-105`}>
-        <div className="md:px-6 pt-3 md:pt-12">
-          <Image src={product.image} alt={product.name} height={300} width={300} className="ml-2 md:ml-4 h-[200px] md:h-60 w-auto" />
+      <div className={`cursor-pointer max-w-[250px] w-[full] md:w-[290px] md:h-[350px] rounded-2xl shadow-card md:shadow-cardCombined ${product.bgColor} flex flex-col items-center justify-between transition-transform hover:scale-105`}>
+        <div className="md:px-6 px-auto pt-4 w-full md:pt-12 flex items-center justify-center">
+          <Image src={product.image} alt={product.name} height={300} width={300} className="ml-3 md:ml-4 h-[200px] md:h-60 w-auto" />
         </div>
         <div className={`flex h-[50px] md:h-[40px] bg-purple1 w-full flex-col items-center justify-center px-4 rounded-b-xl md:rounded-b-2xl md:shadow-card`}>
           <h3 className="text-center text-sm md:text-md font-normal md:font-medium text-white">{product.name}</h3>
--- src/components/Carousel/ProductCarousel.tsx
+++ src/components/Carousel/ProductCarousel.tsx
@@ -8,12 +8,12 @@ import products from "@/data/ProductData";
 
 const ProductCarousel = () => {
   return (
-    <div className="relative flex flex-col items-center justify-between w-full py-12 px-[9vw] bg-white2">
+    <div className="relative flex flex-col items-center justify-between w-full py-12 px-[5vw] md:px-[9vw] bg-white2">
       {/* Title */}
       <div className="text-3xl font-normal text-purple1 mt-6 mb-6">
         <h2>Our Products</h2>
       </div>
-      <div className="flex flex-row items-center justify-center">
+      <div className="flex flex-col md:flex-row items-center justify-center">
         {/* Carousel */}
         <div>
           <Swiper
@@ -22,27 +22,29 @@ const ProductCarousel = () => {
             slidesPerView={2}
             pagination={{ clickable: true }}
             breakpoints={{
-              640: { slidesPerView: 1 },
+              300: { slidesPerView: 1 },
               1024: { slidesPerView: 2 },
             }}
-            className="h-[60vh] w-[60vw]"
+            className="h-[45vh] md:h-[60vh] w-[99vw] md:w-[60vw] mb-12"
           >
             {products.map((product) => (
-              <SwiperSlide key={product.id} className="flex items-center justify-center">
-                <ProductCard id={product.id} />
+              <SwiperSlide key={product.id} className="flex items-center justify-center w-full">
+                <div className="w-[210px] h-full flex items-center justify-center">
+                  <ProductCard id={product.id} />
+                </div>
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
 
         {/* "See More" Button */}
-        <div className="w-full h-full flex items-center justify-center">
+        <div className="w-full h-full flex items-center justify-center mb-12">
           <Link href="/product">
-            <button className="bg-purple1 text-white pl-4 pr-2 py-2 rounded-full shadow-md flex items-center justify-between">
+            <button className="bg-purple1 text-white pl-4 pr-2 py-2 rounded-full shadow-lg flex items-center justify-between">
               <div>
                 See More
               </div>
-              <div className="rounded-full bg-white w-10 h-10 flex items-center justify-center ml-2 text-purple1 font-bold text-lg">
+              <div className="rounded-full bg-white w-8 md:w-10 h-8 md:h-10 flex items-center justify-center ml-2 text-purple1 font-bold text-lg">
                 →
               </div>
             </button>
