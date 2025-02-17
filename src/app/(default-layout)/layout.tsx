6d8ba7b37f8dba874427401555db6a3287ccebdc
NopalAul <86586142+NopalAul@users.noreply.github.com>
2025-02-17 23:33:14 +0700

    feat: metadata

--- src/app/(default-layout)/layout.tsx
+++ src/app/(default-layout)/layout.tsx
@@ -7,15 +7,43 @@ import Navbar from "@/components/Navigation/Navbar";
 import Footer from "@/components/Navigation/Footer";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://kenkovit.com"),
-  keywords: ["Kenkovit", "Kenkovit Website", "Kenkovit Company", "Kenkovit Beauty Collagen", "Collagen"],
+  metadataBase: new URL("https://www.kenkovit.com"),
   title: {
-    default: "Kenkovit",
+    default: "Kenkovit - Premium Beauty & Health Supplements",
     template: "%s | Kenkovit",
   },
-  description: "Kenkovit Website",
+  description: "Discover Kenkovit’s premium beauty collagen and health supplements. Achieve youthful skin, better wellness, and radiant beauty with our trusted products.",
+  keywords: [
+    "Kenkovit", "Kenkovit Beauty", "Collagen Supplement", "Health Supplements",
+    "Skincare", "Anti-aging", "Beauty Products", "Wellness", "Hydrolyzed Collagen", "Kenkovit Company", "Collagen", "Hand Sanitizer", "Kenkovit Indonesia", "Astaxanthin"
+  ],
   icons: [{ rel: "icon", url: "/kenkovit.ico" }],
+  
+  openGraph: {
+    type: "website",
+    url: "https://www.kenkovit.com",
+    title: "Kenkovit - Premium Beauty & Health Supplements",
+    description: "Discover Kenkovit’s premium beauty collagen and health supplements for a youthful and radiant glow.",
+    siteName: "Kenkovit",
+    images: [
+      {
+        url: "https://www.kenkovit.com/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Kenkovit - Beauty & Health Supplements",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    site: "@kenkovit",
+    title: "Kenkovit - Premium Beauty & Health Supplements",
+    description: "Kenkovit offers premium beauty collagen and health supplements for a youthful and radiant glow.",
+    images: ["https://www.kenkovit.com/og-image.jpg"],
+  },
 };
+
 
 const poppins = Poppins({
   subsets: ["latin"],
